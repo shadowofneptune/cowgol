cf21c172e308e1f4dd30744a0646c1992e2c8bbc
shadowofneptune <shadowofneptune@gmail.com>
2023-07-28 17:25:37 -0700

    Cgen passes goto test.   - Changed goto test to make more sense: (no multiply nested loops).   - Changed how cowlink works with tailcalls.

--- rt/cgen/cowgol.h
+++ rt/cgen/cowgol.h
@@ -13,6 +13,8 @@
 #error "Sorry, cowgol cgen doesn't work on big endian machines yet."
 #endif
 
+#define TRAMPOLINE_STACK_SIZE 128
+
 typedef uint8_t i1;
 typedef uint16_t i2;
 typedef uint32_t i4;
@@ -45,6 +47,60 @@ static i8 ram[0x400000 / 8];
 i8* __lomem = (i8*) ram;
 i8* __himem = (i8*) ((i1*)ram + sizeof(ram));
 i8* global_argv;
+
+/* A rather thorny bit of logic controls how tail calls operate.
+ * Trampoline frames allows for multiple levels of the goto statement.
+ * The i1s are mark different states in the execution of the trampoline.
+ * It's a relatively expensive system in total, but most of it is only
+ * incurred if the goto statement is in use. */
+typedef struct {
+/*:The function called by the goto statement: */
+	void(*destination)(void);
+/*1 if a goto statement is in use, 0 otherwise: */
+	i1 activated;
+/*1 if goto->trampoline, 0 if normal return->trampoline. */
+	i1 passer2tramp;
+/*1 if trampoline->function, 0 if function->function */
+	i1 tramp2function;
+} trampoline_frame;
+
+
+trampoline_frame tframes[TRAMPOLINE_STACK_SIZE];
+trampoline_frame* tframe_ptr = tframes;
+
+/* The trampoline itself. It initializes a new frame, then calls the
+ * first destination of the goto chain. Subsequent goto statements
+ * in 'dest' return to it. A return statement without the use of TAILCALL
+ * in 'dest' breaks out of the loop.*/
+void trampoline(void)
+{
+	tframe_ptr++;
+	tframe_ptr->passer2tramp = 1;
+	for(;;) {
+		if (tframe_ptr->passer2tramp) {
+			tframe_ptr->passer2tramp = 0;
+			tframe_ptr->tramp2function = 1;
+			tframe_ptr->destination();
+		} else {
+			tframe_ptr--;
+			return;
+		}
+	}	
+}
+
+/* The return statement within serves two purposes. First, it acts as a way
+ * to return to the trampoline. When the trampoline exits, it acts as
+ * a normal return.*/
+#define TAILCALL(dest)\
+{\
+	tframe_ptr->destination = (dest);\
+	if (!(tframe_ptr->activated)) {\
+		trampoline();\
+	} else {\
+		tframe_ptr->passer2tramp = 1;\
+	}\
+	return;\
+}
 
 extern void cmain(void);
 int main(int argc, const char* argv[])
